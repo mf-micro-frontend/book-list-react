9caae74d765e60564497088f12fe03b86d5c5cf6
BajicMladen <bajicmladen33@gmail.com>
2025-01-25 15:55:53 +0100

    Add tailwind and module federation plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    federation({
+      name: "book-list",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App",
+      },
+      shared: ["react", "react-dom", "tailwindcss"],
+    }),
+  ],
+  build: {
+    port: 5002,
+    target: "esnext",
+  },
+});
